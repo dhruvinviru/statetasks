2fc2336cd9a2eb40b830c966de6a62460b4b3756
dhruvinviru <dhruvinviru@gmail.com>
2026-05-08 11:13:13 +0530

    app.css edited for header dynamic

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from './Logo';
 
 const Header = (props) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
         <header>
             <div className="header-brand">
                 <Logo />
-                <nav>
+                <nav className="desktop-nav">
                     <ul>
                         {props.menu.map((item, index) => (
                             <li key={index}>
                                 <Link to={item.path}>{item.name}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <button className="hamburger" onClick={toggleMenu} aria-label="Toggle menu">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+            </div>
+            <div className={`sidebar ${isMenuOpen ? 'open' : ''}`}>
+                <nav>
+                    <ul>
+                        {props.menu.map((item, index) => (
+                            <li key={index}>
+                                <Link to={item.path} onClick={closeMenu}>{item.name}</Link>
                             </li>
                         ))}
                     </ul>
